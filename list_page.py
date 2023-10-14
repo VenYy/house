817cc4d9b8152f008000442aea21079a8ee4a023
venyy <2815458556@qq.com>
2023-10-14 16:10:26 +0800

    优化翻页

--- list_page.py
+++ list_page.py
@@ -6,46 +6,50 @@ from models import House
 list_page = Blueprint("list_page", __name__)
 
 
-@list_page.route("/query", methods=["GET", "POST"])
+@list_page.route("/query/", methods=["GET", "POST"])
 def search_result():
+    # if request.args.get("addr"):
+    #     addr = request.args.get("addr")
+    #     house_data = House.query.filter(House.address == addr).limit(10).all()
+    #     return render_template("search_list.html", house_data=house_data)
     if request.args.get("addr"):
         addr = request.args.get("addr")
-        house_data = House.query.filter(House.address == addr).limit(10).all()
-        return render_template("search_list.html", house_data=house_data)
+        house_data = House.query.filter(House.address == addr)
+        house_num = house_data.count()
+        total_page_num = math.ceil(house_num / 10)
+        current_page = request.args.get("page", 1, type=int)
+        pagination = house_data.paginate(page=current_page, per_page=10)
+        return render_template("search_list.html",
+                               pagination=pagination,
+                               house_list=pagination.items,
+                               page_num=pagination.page,
+                               total_page_num=total_page_num)
 
     if request.args.get("rooms"):
         rooms = request.args.get("rooms")
-        house_data = House.query.filter(House.rooms == rooms).limit(10).all()
-        return render_template("search_list.html", house_data=house_data)
+        house_data = House.query.filter(House.rooms == rooms)
+        house_num = house_data.count()
+        total_page_num = math.ceil(house_num / 10)
+        current_page = request.args.get("page", 1, type=int)
+        pagination = house_data.paginate(page=current_page, per_page=10)
+        return render_template("search_list.html",
+                               pagination=pagination,
+                               house_list=pagination.items,
+                               page_num=pagination.page,
+                               total_page_num=total_page_num)
     return redirect("/")
-
-
-# @list_page.route("/list/pattern/<int:current_page>")
-# def return_new_list(current_page):
-#     house_num = House.query.count()  # 房源总数量
-#     total_page_num = math.ceil(house_num / 10)  # 总的页码数
-#     pagination = House.query.order_by(
-#         House.publish_time.desc()
-#     ).paginate(page=current_page, per_page=10)
-#
-#     return render_template("list.html",
-#                            pagination=pagination,
-#                            house_list=pagination.items,
-#                            page_num=pagination.page,
-#                            total_page_num=total_page_num
-#                            )
 
 
 @list_page.route("/list/pattern/")
 def return_new_list():
     house_num = House.query.count()  # 房源总数量
     total_page_num = math.ceil(house_num / 10)  # 总的页码数
-    current_page = request.args.get("current_page", 1, type=int)
+    current_page = request.args.get("page", 1, type=int)
     pagination = House.query.order_by(
         House.publish_time.desc()
     ).paginate(page=current_page, per_page=10)
 
-    return render_template("list.html",
+    return render_template("new_list.html",
                            pagination=pagination,
                            house_list=pagination.items,
                            page_num=pagination.page,
@@ -56,12 +60,12 @@ def return_new_list():
 def return_hot_list():
     house_num = House.query.count()  # 房源总数量
     total_page_num = math.ceil(house_num / 10)  # 总的页码数
-    current_page = request.args.get("current_page", 1, type=int)
+    current_page = request.args.get("page", 1, type=int)
     pagination = House.query.order_by(
         House.page_views.desc()
     ).paginate(page=current_page, per_page=10)
 
-    return render_template("list.html",
+    return render_template("hot_list.html",
                            pagination=pagination,
                            house_list=pagination.items,
                            page_num=pagination.page,
